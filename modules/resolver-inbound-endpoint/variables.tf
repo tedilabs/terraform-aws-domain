47ce7bbf48b568c6513e1a5da48a208b5784266c
Byungjin Park <posquit0.bj@gmail.com>
2022-07-28 02:22:35 +0900

    Replace resource_group to resource-group module

--- modules/resolver-inbound-endpoint/variables.tf
+++ modules/resolver-inbound-endpoint/variables.tf
@@ -1,16 +1,17 @@
 variable "name" {
-  description = "The name of the Route53 Resolver Inbound Endpoint."
+  description = "(Required) The name of the Route53 Resolver Inbound Endpoint."
   type        = string
 }
 
 variable "security_groups" {
-  description = "A list of security groups for Route53 Resolver Inbound Endpoint."
+  description = "(Optional) A list of security groups for Route53 Resolver Inbound Endpoint."
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "ip_allocations" {
-  description = "A list of IP allocations which include `subnet_id` and `ip`. `ip` is a private IP from `subnet`."
+  description = "(Required) A list of IP allocations which include `subnet_id` and `ip`. `ip` is a private IP from `subnet`."
   type = list(object({
     subnet_id = string
     ip        = string
@@ -23,21 +24,24 @@ variable "ip_allocations" {
 }
 
 variable "allowed_ingress_cidrs" {
-  description = "A list of CIDR for Route53 Resolver Inbound Endpoint ingress access."
+  description = "(Optional) A list of CIDR for Route53 Resolver Inbound Endpoint ingress access."
   type        = list(string)
   default     = ["0.0.0.0/0"]
+  nullable    = false
 }
 
 variable "tags" {
-  description = "A map of tags to add to all resources."
+  description = "(Optional) A map of tags to add to all resources."
   type        = map(string)
   default     = {}
+  nullable    = false
 }
 
 variable "module_tags_enabled" {
-  description = "Whether to create AWS Resource Tags for the module informations."
+  description = "(Optional) Whether to create AWS Resource Tags for the module informations."
   type        = bool
   default     = true
+  nullable    = false
 }
 
 
@@ -46,19 +50,22 @@ variable "module_tags_enabled" {
 ###################################################
 
 variable "resource_group_enabled" {
-  description = "Whether to create Resource Group to find and group AWS resources which are created by this module."
+  description = "(Optional) Whether to create Resource Group to find and group AWS resources which are created by this module."
   type        = bool
   default     = true
+  nullable    = false
 }
 
 variable "resource_group_name" {
-  description = "The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
+  description = "(Optional) The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
   type        = string
   default     = ""
+  nullable    = false
 }
 
 variable "resource_group_description" {
-  description = "The description of Resource Group."
+  description = "(Optional) The description of Resource Group."
   type        = string
   default     = "Managed by Terraform."
+  nullable    = false
 }
