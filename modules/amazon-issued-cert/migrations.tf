7446e091331633d2c31d9ef95170fdcede13e1c9
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2025-10-31 14:56:39 +0900

    feat: migrate acm related modules to terraform-aws-cert (#60)

--- modules/amazon-issued-cert/migrations.tf
+++
@@ -1,5 +0,0 @@
-# INFO: [2023-11-17] Change the name of `aws_acm_certificate_validation` resource for DNS validation
-moved {
-  from = aws_acm_certificate_validation.this
-  to   = aws_acm_certificate_validation.dns
-}
